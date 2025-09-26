4681eba50dbaa8bc92994f789ba074e956147bac
jpangi <jacob.Pangilinan10@gmail.com>
2025-09-25 18:00:16 -0700

    added show page

--- server.js
+++ server.js
@@ -25,15 +25,25 @@ app.get('/', (req,res)=>{
     res.render("index.ejs")
 })
 
+app.get('/trips', async (req,res)=>{
+    const allTrips = await Trip.find();
+    res.render('./trips/index.ejs', {trips: allTrips})
+})
+
 app.get('/trips/new', (req,res)=>{
     res.render('trips/new.ejs')
 })
 
 app.post('/trips', async(req,res)=>{
     await Trip.create(req.body)
-    res.send('New Trip created')
+    res.redirect('/trips')
 })
 
+app.get('/trips/:tripId', async (req,res)=>{
+    const foundTrip = await Trip.findById(req.params.tripId)
+    console.log('SHOW PAGE', foundTrip);
+    res.render('trips/show.ejs', {trip: foundTrip})
+})
 
 const PORT = process.env.PORT
 app.listen(PORT, (req,res)=>{
